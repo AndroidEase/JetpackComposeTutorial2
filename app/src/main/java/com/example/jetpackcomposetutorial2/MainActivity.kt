81bc6f78232a8d05afe545a7549ddd6509ee1b6d
DharaSolanki <dhara.solanki@thegatewaydigital.com>
2024-07-31 11:45:23 +0530

    -Abc

--- app/src/main/java/com/example/jetpackcomposetutorial2/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial2/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial2.ui.theme.JetpackComposeTutorial2Theme
 
 class MainActivity : ComponentActivity() {
@@ -32,9 +39,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    val clickOnText = {
+        println("Text Clicked")
+    }
+
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Ad ${stringResource(id = R.string.hello_world)}",
+        color = Color.Red,
+        fontSize = 32.sp,
+        fontStyle = FontStyle.Italic,
+        modifier = Modifier.padding(22.dp).background(Color.Black).clickable(onClick = clickOnText)
     )
 }
 
